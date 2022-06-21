9ec5dce0ab4a9f273fd62d06e3eaeba69012344f
blankhall98 <blankhall@ciencias.unam.mx>
2022-06-20 22:32:48 -0500

    Simple Test

--- scripts/matcher.py
+++ scripts/matcher.py
@@ -12,13 +12,6 @@ class Matcher:
         self.read_data()
         self.reduce_base()
 
-        print(self.ppb.columns)
-        print(self.nuc.columns)
-
-        print(self.ppb.head())
-        print(self.nuc.head())
-
-
     #Read PPB and NUC databases
     def read_data(self):
         #routes
@@ -39,13 +32,15 @@ class Matcher:
         j = len(self.nuc)
         n = len(self.ppb)
         for i in range(len(self.ppb)):
-            if i % 1000 == 0:
+            if i % 10 == 0:
                 print(f'Progress: {i/n}% ...')
             matched_lvl1 = False
             counter = 0
             #lets first enter the level 1 region
             while not matched_lvl1 and counter < j:
-                matched_lvl1 = self.tester.test(self.ppb.loc[i]['MUNICIPIO'],self.nuc.loc[counter]['MUNICIPIO'])
+                a = self.ppb.loc[i]['MUNICIPIO']
+                b = self.nuc.loc[counter]['MUNICIPIO']
+                matched_lvl1 = self.tester.test(a,b)
                 counter = counter + 1
             
             #decide what to do depending if we found lvl 1 match
@@ -58,7 +53,9 @@ class Matcher:
                 counter = 0
                 #lvl2 region
                 while not matched_lvl2 and counter < j:
-                    matched_lvl2 = self.tester.test(self.ppb.loc[i]['NUCLEO_AGRARIO'],self.nuc.loc[counter]['NOM_NUC'])
+                    a = self.ppb.loc[i]['NUCLEO_AGRARIO']
+                    b = self.nuc.loc[counter]['NOM_NUC']
+                    matched_lvl2 = self.tester.test(a,b)
                     counter = counter + 1
                 #no match
                 if not matched_lvl2:
@@ -74,4 +71,4 @@ class Matcher:
 
     def save(self):
         saving_route = self.inputs['processed_route'] + self.inputs['matched_filename']
-        self.ppb.to_csv()
+        self.ppb.to_csv(saving_route)
